f0c55442bbc3614f95949a9cf2434f570553b5b0
Demetrios Loizides <loizidesdemetris@gmail.com>
2023-12-27 01:17:37 +0200

    proto type of loading image

--- family-gallery/src/app/folder/folder.page.ts
+++ family-gallery/src/app/folder/folder.page.ts
@@ -7,4 +7,26 @@ import { Component } from '@angular/core';
 })
 export class FolderPage {
 
+  public files: File[] = [];
+  public currentImageUrl = '';
+
+  public chooseFile(event: any) {
+    console.log(event);
+    if (event.target.files && event.target.files[0]) {
+      this.files = Array.from(event.target.files);
+    }
+    this.startPreview();
+  }
+
+  public startPreview() {
+    this.files.forEach((image, index) => {
+      const reader = new FileReader();
+      reader.onload = (ev) => {
+        if ((typeof reader.result) === 'string') {
+          this.currentImageUrl = ev.target?.result as string;
+        }
+      };
+      reader.readAsDataURL(image);
+    });
+  }
 }
