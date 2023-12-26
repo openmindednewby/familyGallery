c468480aea6d2f8ad28e56bdf2b2459b8bda4065
Demetrios Loizides <loizidesdemetris@gmail.com>
2023-12-26 21:26:48 +0200

    app creation and support for ios and android

--- family-gallery/src/app/folder/folder.page.ts
+++ family-gallery/src/app/folder/folder.page.ts
@@ -1,17 +1,10 @@
-import { Component, inject, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-folder',
   templateUrl: './folder.page.html',
   styleUrls: ['./folder.page.scss'],
 })
-export class FolderPage implements OnInit {
-  public folder!: string;
-  private activatedRoute = inject(ActivatedRoute);
-  constructor() {}
+export class FolderPage {
 
-  ngOnInit() {
-    this.folder = this.activatedRoute.snapshot.paramMap.get('id') as string;
-  }
 }
